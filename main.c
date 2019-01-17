0bb96a00930bfc6a68ac168ed642a8e9be759af5
Your Name <you@example.com>
2019-01-17 11:00:20 +0800

    基本框架的构建

--- main.c
+++ main.c
@@ -1,9 +1,38 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
 
-    printf("hello world!");
+    printf("jinrudachexitong \n");
+    while (1)
+    {
+        printf("1---tianjiacheliangweizhi \n");
+        printf("2---shanchucheliangweizhi \n");
+        printf("3---xianshiquanbucheliangweizhi \n");
+        printf("4---fanhuiliwoweizhizuijindecheliang \n");
+        printf("5---tuichu \n");
+        printf("qingxuanzexiangtingdegongnengbianhao \n");
+        int code;
+        scanf("%d", &code);
+
+        if (code == 1)
+        {
+        }
+        if (code == 2)
+        {
+        }
+        if (code == 3)
+        {
+        }
+        if (code == 4)
+        {
+        }
+        if (code == 5)
+        {
+            printf("jieshu,dianjiuichetuichu \n");
+            break;
+        }
+    }
 
     return 0;
-
 }
