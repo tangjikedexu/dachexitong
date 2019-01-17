e3070bd3583c7c4eb9c14818ac966c81ca0c1116
Your Name <you@example.com>
2019-01-17 11:24:09 +0800

    添加显示所有车辆位置系统的构建

--- main.c
+++ main.c
@@ -18,6 +18,7 @@ int main()
         printf("3---xianshiquanbucheliangweizhi \n");
         printf("4---fanhuiliwoweizhizuijindecheliang \n");
         printf("5---tuichu \n");
+
         printf("qingxuanzexiangtingdegongnengbianhao \n");
         int code;
         scanf("%d", &code);
@@ -29,9 +30,12 @@ int main()
             int y;
             scanf("%d", &x);
             scanf("%d", &y);
+
             arr[index].x = x;
             arr[index].y = y;
+
             index++;
+
             printf("tianjiachenggong,dianjihuichejixu \n");
             char xx;
             scanf("%c", &xx);
@@ -39,9 +43,18 @@ int main()
         }
         if (code == 2)
         {
+
         }
         if (code == 3)
         {
+            printf("suoyoucheliangweizhiruxia: \n");
+            for(int i=0;i<index;i++){
+                printf("di%dgecheliangdeweizhiwei(%d,%d)\n",i+1,arr[i].x,arr[i].y);
+            }
+            printf("\n dianjihuichejixu \n");
+            char xx;
+            scanf("%c", &xx);
+            scanf("%c", &xx);
         }
         if (code == 4)
         {
